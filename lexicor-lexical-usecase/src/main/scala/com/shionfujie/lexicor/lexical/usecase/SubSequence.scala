840725be9ad8cb8e036f770d10a361c939697575
shion.t.fujie <tonaluna32966@gmail.com>
2019-09-03 08:40:14 +0900

    Add SubSequence

---
+++ lexicor-lexical-usecase/src/main/scala/com/shionfujie/lexicor/lexical/usecase/SubSequence.scala
@@ -0,0 +1,23 @@
+package com.shionfujie.lexicor.lexical.usecase
+
+/** A shallow wrapper over another CharSequence (usually a String)
+  * See SI-7710: in jdk7u6 String.subSequence stopped sharing the char array of the original
+  * string and began copying it.
+  * RegexParsers calls subSequence twice per input character: that's a lot of array copying!
+  */
+private class SubSequence(s: CharSequence, start: Int, val length: Int) extends CharSequence {
+  def this(s: CharSequence, start: Int) = this(s, start, s.length - start)
+
+  def charAt(i: Int) =
+    if (i >= 0 && i < length) s.charAt(start + i)
+    else throw new IndexOutOfBoundsException(s"index: $i, length: $length")
+
+  def subSequence(_start: Int, _end: Int) = {
+    if (_start < 0 || _end < 0 || _end > length || _start > _end)
+      throw new IndexOutOfBoundsException(s"start: ${_start}, end: ${_end}, length: $length")
+
+    new SubSequence(s, start + _start, _end - _start)
+  }
+
+  override def toString = s.subSequence(start, start + length).toString
+}
