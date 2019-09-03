749094b2509f8b2c598481087567fde2b3b0d7c9
shion.t.fujie <tonaluna32966@gmail.com>
2019-09-03 21:14:56 +0900

    SyntaxParser.test("illegal predicate")

--- lexicor-syntax-usecase/src/test/scala/com/shionfujie/lexicor/syntax/usecase/SyntaxParserTest.scala
+++ lexicor-syntax-usecase/src/test/scala/com/shionfujie/lexicor/syntax/usecase/SyntaxParserTest.scala
@@ -16,6 +16,7 @@ class SyntaxParserTest extends FunSuite with Matchers {
     val `tag is #[Target]` = "tag is #[Target]"
     val `tag is in #[First/Second third]` = "tag is in #[First/Second third]"
     val `tag` = "tag"
+    val `tag fly #[Target]` = "tag fly #[Target]"
   }
 
   /** Test cases each entry of which consists of a string sentence written in the language on the left hand side and its
@@ -24,6 +25,7 @@ class SyntaxParserTest extends FunSuite with Matchers {
     `tag is #[Target]` -> List(Subject((0, 2), Symbols.Tag), Keyword((4, 5), Symbols.Is), TagLiteral((7, 20), List("Target"))),
     `tag is in #[First/Second third]` -> List(Subject((22, 24), Symbols.Tag), Keyword((26, 27), Symbols.Is), Keyword((19, 21), Symbols.In), TagLiteral((33, 51), List("First", "Second third"))),
     `tag` -> List(Subject((0, 2), Symbols.Tag)),
+    `tag fly #[Target]` -> List(Subject((0, 2), Symbols.Tag), Unknown((4, 7)), TagLiteral((9, 22), List("Target"))),
   )
 
   test("parse") {
@@ -42,6 +44,10 @@ class SyntaxParserTest extends FunSuite with Matchers {
 
   test("missing predicate") {
     lexemesOf(`tag`) shouldFailBecauseOf new Error(at = (3, 4), message = "predicate expected")
+  }
+
+  test("illegal predicate") {
+    lexemesOf(`tag fly #[Target]`) shouldFailBecauseOf new Error(at = (4, 22), message = "illegal predicate")
   }
 
   private class LexemesShouldFailBecauseOfReason(syntaxParser: SyntaxParser, lexemes: List[Lexeme]) {
