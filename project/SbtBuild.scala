49bde58866ee257fb872b573a30e546c690ae4e2
shion.t.fujie <tonaluna32966@gmail.com>
2019-09-04 13:19:42 +0900

    scala pb common settings

--- project/SbtBuild.scala
+++ project/SbtBuild.scala
@@ -1,4 +1,6 @@
 import sbt._
+import sbt.Keys.sourceManaged
+import sbtprotoc.ProtocPlugin.autoImport.PB
 
 object buildConfig {
   val organization = "com.shionfujie"
@@ -13,5 +15,12 @@ object deps {
   val scalaPBDependencies = Seq(
     "io.grpc" % "grpc-netty" % scalapb.compiler.Version.grpcJavaVersion,
     "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapb.compiler.Version.scalapbVersion
+  )
+}
+
+object scalaPBSettings {
+  def apply = Seq(
+    PB.targets in Compile :=
+      Seq(scalapb.gen(flatPackage = true) -> (sourceManaged in Compile).value)
   )
 }
