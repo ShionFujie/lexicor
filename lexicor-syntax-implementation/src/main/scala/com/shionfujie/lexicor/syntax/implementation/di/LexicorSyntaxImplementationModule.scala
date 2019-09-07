2e6385ea591027791ffe51c7b75c6528f24d3b03
shion.t.fujie <tonaluna32966@gmail.com>
2019-09-07 12:28:37 +0900

    Add LexicorSyntaxImplementationModule

---
+++ lexicor-syntax-implementation/src/main/scala/com/shionfujie/lexicor/syntax/implementation/di/LexicorSyntaxImplementationModule.scala
@@ -0,0 +1,16 @@
+package com.shionfujie.lexicor.syntax.implementation.di
+
+import com.shionfujie.lexicor.syntax.implementation.SyntaxParseService
+
+final class LexicorSyntaxImplementationModule(
+                                               usecaseModule: LexicorSyntaxUsecaseModule,
+                                               adapterModule: LexicorSyntaxAdapterModule
+                                             ) {
+
+  import adapterModule._
+  import usecaseModule._
+
+  lazy val parseService: SyntaxParseService =
+    new SyntaxParseService(syntaxParser, requestDeserializer, responseSerializer)
+
+}
