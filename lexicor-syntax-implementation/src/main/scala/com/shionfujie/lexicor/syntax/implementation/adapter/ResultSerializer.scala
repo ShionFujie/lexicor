d72d53044db6feeb2af54682409d948cba870598
shion.t.fujie <tonaluna32966@gmail.com>
2019-09-08 13:21:48 +0900

    Reformat lexicor-syntax-implementation module

--- lexicor-syntax-implementation/src/main/scala/com/shionfujie/lexicor/syntax/implementation/adapter/ResultSerializer.scala
+++ lexicor-syntax-implementation/src/main/scala/com/shionfujie/lexicor/syntax/implementation/adapter/ResultSerializer.scala
@@ -1,13 +1,21 @@
 package com.shionfujie.lexicor.syntax.implementation.adapter
 
 import com.shionfujie.lexicor.core.implementation.adapter.Serializer
-import com.shionfujie.lexicor.syntax.domain.{Cond => DomainCond, Error => DomainError, Result => DomainResult}
-import com.shionfujie.lexicor.syntax.grpc.{Cond => GrpcCond, Error => GrpcError, Result => GrpcResult}
+import com.shionfujie.lexicor.syntax.domain.{
+  Cond => DomainCond,
+  Error => DomainError,
+  Result => DomainResult
+}
+import com.shionfujie.lexicor.syntax.grpc.{
+  Cond => GrpcCond,
+  Error => GrpcError,
+  Result => GrpcResult
+}
 
 class ResultSerializer(
-                        condSerializer: Serializer[DomainCond, GrpcCond],
-                        errorSerializer: Serializer[DomainError, GrpcError]
-                      ) extends Serializer[DomainResult, GrpcResult] {
+    condSerializer: Serializer[DomainCond, GrpcCond],
+    errorSerializer: Serializer[DomainError, GrpcError]
+) extends Serializer[DomainResult, GrpcResult] {
 
   override def serialize(result: DomainResult): GrpcResult = result match {
     case Left(error) => GrpcResult().withError(errorSerializer.serialize(error))
--- lexicor-syntax-implementation/src/main/scala/com/shionfujie/lexicor/syntax/implementation/adapter/SyntaxParseReplySerializer.scala
+++ lexicor-syntax-implementation/src/main/scala/com/shionfujie/lexicor/syntax/implementation/adapter/SyntaxParseReplySerializer.scala
@@ -5,7 +5,7 @@ import com.shionfujie.lexicor.syntax.domain.{Result => DomainResult}
 import com.shionfujie.lexicor.syntax.grpc.{SyntaxParseReply, Result => GrpcResult}
 
 class SyntaxParseReplySerializer(resultSerializer: Serializer[DomainResult, GrpcResult])
-  extends Serializer[List[DomainResult], SyntaxParseReply] {
+    extends Serializer[List[DomainResult], SyntaxParseReply] {
 
   override def serialize(results: List[DomainResult]): SyntaxParseReply =
     SyntaxParseReply(results.map(resultSerializer.serialize))
